a7e2ff0d714fc633bd00ad8cda57c277e8d55d6b
Polina Bovykina <paulina.northy@gmail.com>
2024-04-09 22:05:20 +0100

    docker demo

---
+++ src/main/java/com/example/demo/App.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class App {
+
+    public static void main(String[] args) {
+        SimpleController simp = new SimpleController();
+        simp.hello();
+    }
+}
---
+++ src/main/java/com/example/demo/SimpleController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello Docker!";
+    }
+}
